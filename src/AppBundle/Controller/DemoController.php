818fe705b7b08e943feb3c50b2d9984c5c84cdc8
Julian <julian.li@pepperstone.com>
2017-01-21 11:01:18 +1100

    tweak controller

--- src/AppBundle/Controller/DemoController.php
+++ src/AppBundle/Controller/DemoController.php
@@ -1,50 +1,14 @@
 <?php
-
 namespace AppBundle\Controller;
 
-use AppBundle\Entity\User;
 use AppBundle\Form\UserType;
-use Aws\Sns\Exception\SnsException;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Aws\Sns\SnsClient;
 
 class DemoController extends Controller
 {
-    /**
-     * @Route("/sample", name="sample")
-     *
-     */
-    public function indexAction()
-    {
-        $snsClient = new SnsClient(
-            [
-                'version' => $this->getParameter('aws_version'),
-                'region' => $this->getParameter('aws_region'),
-                'credentials' => [
-                    'key' => $this->getParameter('aws_key'),
-                    'secret' => $this->getParameter('aws_secret')
-                ]
-            ]
-        );
-
-        try {
-            $snsClient->publish([
-                'Message' => '{"uer_id":12,"email":"julian.li@pepperstone.com","event":"user_created"}',
-                'Subject' => 'User Created',
-                'TopicArn' => $this->getParameter('aws_sns_topic_arn')
-            ]);
-        } catch (SnsException $e) {
-            dump($e->getMessage());
-            die;
-        }
-
-        return new Response('User Created Message Published');
-    }
-
     /**
      * @Route("/", name="user_registration")
      */
@@ -60,21 +24,13 @@ class DemoController extends Controller
             $em->persist($user);
             $em->flush();
 
-            $snsClient = $this->get('aws_factory')->createClient('sns');
-
-            try {
-                $snsClient->publish([
-                    'Message' => $this->get('jms_serializer')->serialize($user, 'json'),
-                    'Subject' => $this->getParameter('aws_user_created_subject'),
-                    'TopicArn' => $this->getParameter('aws_user_created_arn')
-                ]);
-            } catch (SnsException $e) {
-                dump($e);
-                //put exception into log or send email.
-            }
+            $this->get('aws.sns.helper')->publish(
+                $this->get('jms_serializer')->serialize($user, 'json'),
+                $this->getParameter('aws_user_created_subject'),
+                $this->getParameter('aws_user_created_arn')
+            );
 
             $this->addFlash('success', 'User created!');
-
             return $this->redirectToRoute('user_registration');
         }
 
---
+++ src/AppBundle/Service/SNSHelper.php
@@ -0,0 +1,34 @@
+<?php
+namespace AppBundle\Service;
+
+
+use Aws\Sns\Exception\SnsException;
+use Aws\Sns\SnsClient;
+
+class SNSHelper
+{
+    private $client;
+
+    public function __construct(SnsClient $client)
+    {
+        $this->client = $client;
+    }
+
+    public function publish($message, $subject, $arn)
+    {
+        try {
+            $this->client->publish([
+                'Message' => $message,
+                'Subject' => $subject,
+                'TopicArn' => $arn
+            ]);
+
+        } catch (SnsException $e) {
+            dump($e);
+            //put exception into log or send email.
+        }
+
+
+    }
+
+}
