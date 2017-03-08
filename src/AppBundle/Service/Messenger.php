fccd9d41fd66f32eab4c15b3f06e1370bd6e93b1
Julian <julian.li@pepperstone.com>
2017-03-08 17:40:00 +1100

    fix typo

--- src/AppBundle/Service/Messenger.php
+++ src/AppBundle/Service/Messenger.php
@@ -2,13 +2,13 @@
 namespace AppBundle\Service;
 
 use AppBundle\Messaging\MessageProcessInterface;
-use AppBundle\Service\Aws\SQSHelper;
+use AppBundle\Service\Aws\SQS;
 
 class Messenger
 {
     private $client;
 
-    public function __construct(SQSHelper $client)
+    public function __construct(SQS $client)
     {
         $this->client = $client;
     }
