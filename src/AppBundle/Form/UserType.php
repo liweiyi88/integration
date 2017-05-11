f52d5c2533cc05604e7b135231f006717eb24917
Julian Li <weiyi.li713@gmail.com>
2017-05-11 18:06:01 +1000

    change style

--- src/AppBundle/Form/UserType.php
+++ src/AppBundle/Form/UserType.php
@@ -4,7 +4,9 @@ namespace AppBundle\Form;
 
 use AppBundle\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,8 +15,14 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('username')
-            ->add('email');
+            ->add('username', TextType::class, array(
+                'label' => 'Username',
+                'label_attr' => ['class' => 'mdc-textfield__label' ]
+            ))
+            ->add('email', EmailType::class, array(
+                'label' => 'Email',
+                'label_attr' => ['class' => 'mdc-textfield__label']
+            ));
     }
 
     public function configureOptions(OptionsResolver $resolver)
