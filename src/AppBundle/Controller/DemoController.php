22ef2fd245ef0b786cae98ed5ff27d4cbf279bbb
Julian Li <liweiyi88@users.noreply.github.com>
2017-05-18 17:31:39 +1000

    add Queue (#1)
    * add Queue
    
    * add codeclimate.yml
    
    * remove one check
    
    * add exclude paths
    
    * remove naming check

--- src/AppBundle/Controller/DemoController.php
+++ src/AppBundle/Controller/DemoController.php
@@ -1,6 +1,8 @@
 <?php
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Queue;
+use AppBundle\Entity\User;
 use AppBundle\Form\UserType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
@@ -18,10 +20,25 @@ class DemoController extends Controller
 
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
+            /** @var User $user */
             $user = $form->getData();
 
             $em = $this->getDoctrine()->getManager();
             $em->persist($user);
+
+            //The Queue entity is not necessary. It is just used to make it easier to show the process behind the sense.
+            $confirmationQueue = new Queue();
+            $confirmationQueue->setName('confirmation');
+            $confirmationQueue->setUsername($user->getUsername());
+            $confirmationQueue->setEmail($user->getEmail());
+
+            $mailchimpQueue = new Queue();
+            $mailchimpQueue->setName('mailchimp');
+            $mailchimpQueue->setUsername($user->getUsername());
+            $mailchimpQueue->setEmail($user->getEmail());
+
+            $em->persist($confirmationQueue);
+            $em->persist($mailchimpQueue);
             $em->flush();
 
             $this->get('aws.sns.helper')->publish(
---
+++ src/AppBundle/Entity/Queue.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Queue
+ *
+ * @ORM\Table(name="queue")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\QueueRepository")
+ */
+class Queue
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="username", type="string", length=255)
+     */
+    private $username;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="email", type="string", length=255)
+     */
+    private $email;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Queue
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set username
+     *
+     * @param string $username
+     *
+     * @return Queue
+     */
+    public function setUsername($username)
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    /**
+     * Get username
+     *
+     * @return string
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * Set email
+     *
+     * @param string $email
+     *
+     * @return Queue
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+}
+
